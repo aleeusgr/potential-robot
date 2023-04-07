b93ce74586d6bc8fbde2f6bb2b824eeacf3392e8
Alex <alexeusgr@gmail.com>
2023-04-07 09:31:01 +0200

    replace simple harness with example by Giovanni

--- test/simple.test.js
+++ test/simple.test.js
@@ -1,31 +1,69 @@
 import * as helios from "@hyperionbt/helios"
+import { describe, it, beforeAll, expect } from 'vitest'
 import fs from "fs";
-import {Color} from '../colors.js'
+// import {Color} from '../colors.js'
 
-// 
-const contract = fs.readFileSync("./test/simple.helios").toString();
-// test helpers and fixtures can be loaded from a different files and concatenated
-// const helpers = fs.readFileSync("./test_helpers.helios").toString();
-// const fixtures = fs.readFileSync("./simple_fixtures.helios").toString();
-// const program = helios.Program.new(contract + helpers + fixtures);
-const program = helios.Program.new(contract);
-const testContract = program.compile();
+let program;
+let testContract;
+
+describe('Raffle Vault works appropriately', () => {
+
+    beforeAll(() => {
+
+        const testSetup = {
+            contract: './test/simple.hl',
+            // fixtures: [
+            //     './tests/vault_fixtures.hl'
+            // ],
+            helpers: [
+            ],
+            // replacements: [
+            //     []
+            // ]
+        }
+
+        const contractSources: string[] = []
+
+        contractSources.push(
+            fs.readFileSync(testSetup.contract).toString()
+        )
+
+        // testSetup.fixtures.forEach(fixture => contractSources.push(fs.readFileSync(fixture).toString()))
+
+        // const source = contractSources
+        //     .join("\n")
+        //     .replace('context.get_current_validator_hash()', 'ValidatorHash::new(#01234567890123456789012345678901234567890123456789000001)')
+
+        program = Program.new(source);
+        testContract = program.compile();
+    })
 
 // evalParam(p) and runWithPrint(p[]). Look for unit "()" response
-async function testSuccess(testName, paramNames) {
-  const args = paramNames.map((p) => program.evalParam(p));
-  testContract.runWithPrint(args).then((res) => {
-      const assertion = res[0].toString() == "()";
-      if (assertion) {
-	console.log(`${Color.FgGreen}Test ${testName} was successful!${Color.Reset}`);
-      } else {
-          logFail(testName, res, args);
-      }
+    
+    it(`should ...`, async () => {
+
+        const args = paramNames.map((p) => program.evalParam(p))
+        return await testContract
+            .runWithPrint(args)
+            .then((res) => {
+                console.log('res', JSON.stringify(res))
+                expect(res[0].toString()).toBe("()");
+            })
     })
-    .catch((err) => {
-      logFail(testName, err, args, true);
-    });
-}
+  // async function testSuccess(testName, paramNames) {
+  //   const args = paramNames.map((p) => program.evalParam(p));
+  //   testContract.runWithPrint(args).then((res) => {
+  //       const assertion = res[0].toString() == "()";
+  //       if (assertion) {
+  //         console.log(`${Color.FgGreen}Test ${testName} was successful!${Color.Reset}`);
+  //       } else {
+  //           logFail(testName, res, args);
+  //       }
+  //     })
+  //     .catch((err) => {
+  //       logFail(testName, err, args, true);
+  //     });
+  // }
 
 // async function testFailure(testName, paramNames) {
 //     const args = paramNames.map((p) => program.evalParam(p));
@@ -52,7 +90,10 @@ async function testSuccess(testName, paramNames) {
 //     console.log(`${Color.FgRed}--------------${Color.Reset}`)
 // }
 
-await testSuccess("test1Success", ["empty_datum", "test1_true_redeemer", "default_ctx"]);
+//await testSuccess("test1Success", ["empty_datum", "test1_true_redeemer", "default_ctx"]);
 // await testFailure("test1Failure", ["empty_datum", "test1_false_redeemer", "default_ctx"]);
-await testSuccess("test2Success", ["empty_datum", "test2_true_redeemer", "default_ctx"]);
+//await testSuccess("test2Success", ["empty_datum", "test2_true_redeemer", "default_ctx"]);
 // await testFailure("test2Failure", ["empty_datum", "test2_false_redeemer", "default_ctx"]);
+//
+
+})
