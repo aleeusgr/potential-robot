c1cf2ddf9b44855af221b68ea90b433389033046
Alex <alexeusgr@gmail.com>
2023-04-25 14:37:09 +0200

    enable helios test

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,6 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   test: {
-    include: ['tests/lucid.test.*'],
+    include: ['tests/lucid.test.*', 'tests/mint-onchain-positive.test.*'],
   },
 });
