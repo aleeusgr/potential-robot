94e72cff7e77aeeea06bba58b0a6fece0c35876f
Alex <alexeusgr@gmail.com>
2023-04-26 08:26:44 +0200

    failed experiments

--- tests/lucid.test.ts
+++ tests/lucid.test.ts
@@ -1,7 +1,16 @@
-import { describe, expect, it, beforeEach, expectTypeOf, vi } from 'vitest'
+import { describe, expect, it, vi } from 'vitest'
 import { promises as fs } from 'fs';
 import {
-
+  Assets, 
+  ConstrData, 
+  MintingPolicyHash,
+  NetworkEmulator,
+  NetworkParams,
+  Program, 
+  Value, 
+  textToBytes,
+  TxOutput,
+  Tx, 
 } from "@hyperionbt/helios";
 
 import {
@@ -12,26 +21,20 @@ import {
   generateSeedPhrase,
   getAddressDetails,
   Lucid,
+  SpendingValidator,
+  toUnit,
   TxHash,
 } from "lucid-cardano"; // NPM
 
-describe('TestSuite: ', () => {
-	// single test, 
-	// it checks the status of the main function, while the main() takes care of logging and IO;
-	// API methods:
-	// set up the emulator;
-	// execute a transaction;
-	// Is there a way to add nft into a wallet in the emulate without using minting script?
-	// If not, I need to add a code block that will output a uplc consumable by the transaction builder.
-	// I need to find a method or code example in lucid that will show a transaction with a uplc involved
-	//
+describe('ThreadToken Positive Test Cases', () => {
 
-    beforeEach(async () => {
+    const main = async () => {
 
 	// Set the Helios compiler optimizer flag
         let optimize = false;
         const minAda = BigInt(2000000);  // minimum lovelace needed to send an NFT
-	
+
+        try {
 	// https://github.com/spacebudz/lucid/blob/main/tests/emulator.test.ts
 	async function generateAccount(assets: Assets) {
 	  const seedPhrase = generateSeedPhrase();
@@ -48,45 +51,48 @@ describe('TestSuite: ', () => {
 
 	const emulator = new Emulator([lender, borrower]);
 
-	const lucid = Lucid.new(emulator);
-	
-	//what's this?
-	// lucid.selectWalletFromSeed(lender.seedPhrase);
-	// const recipient = "addr_test1qrupyvhe20s0hxcusrzlwp868c985dl8ukyr44gfvpqg4ek3vp92wfpentxz4f853t70plkp3vvkzggjxknd93v59uysvc54h7";
+	const lucid = await Lucid.new(emulator);
 
-	// const datum = Data.to(123n);
-	// const lovelace = 3000000n;
+	lucid.selectWalletFromSeed(lender.seedPhrase);
+	const recipient =
+    "addr_test1qrupyvhe20s0hxcusrzlwp868c985dl8ukyr44gfvpqg4ek3vp92wfpentxz4f853t70plkp3vvkzggjxknd93v59uysvc54h7";
 
-	// const tx = await lucid.newTx().payToAddressWithData(recipient, {
-	//   inline: datum,
-	// }, { lovelace }).complete();
+	// this needs to change.
+	const datum = Data.to(123n);
+	const lovelace = 3000000n;
 
-	// const signedTx = await tx.sign().complete();
-	// const txHash = await signedTx.submit();
-	// await lucid.awaitTx(txHash);
+	const tx = await lucid.newTx().payToAddressWithData(recipient, {
+	  inline: datum,
+	}, { lovelace }).complete();
 
-	// const utxos = await lucid.utxosAt(
-	//   recipient,
-	// );
+	const signedTx = await tx.sign().complete();
+	const txHash = await signedTx.submit();
+	await lucid.awaitTx(txHash);
+
+	const utxos = await lucid.utxosAt(
+	  recipient,
+	);
         // return true;
-        // const walletLovelace = parseInt(utxos[0].assets.lovelace)
-	return lucid
-    })
+        return parseInt(utxos[0].assets.lovelace) == 3000000
+        } catch (err) {
+            //console.error("something failed:", err);
+            return false;
+        }
+    }
 
-    it(' ', async () => {
+    it('checks main() status', async () => {
 
-        // const logMsgs = new Set();
-        // const logSpy = vi.spyOn(global.console, 'log')
-        //                  .mockImplementation((msg) => { logMsgs.add(msg); });
-        // 
-        // let mainStatus = await main();
-        // logSpy.mockRestore();
-        // if (!mainStatus) {
-        //     console.log("Smart Contract Messages: ", logMsgs);
-        // }
-        // expect(mainStatus).toBe(true);
+        const logMsgs = new Set();
+        const logSpy = vi.spyOn(global.console, 'log')
+                         .mockImplementation((msg) => { logMsgs.add(msg); });
+        
+        let mainStatus = await main();
+        logSpy.mockRestore();
+        if (!mainStatus) {
+            console.log("Smart Contract Messages: ", logMsgs);
+        }
+        expect(mainStatus).toBe(true);
 
-	expect(lucid).toBe(true)
     })
 
 })
