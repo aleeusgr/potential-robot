f31380416debf9da0a6c6855885ccee823d92bec
Alex <alexeusgr@gmail.com>
2023-05-03 07:44:30 +0200

    added redeemUtxo to integration test

--- tests/integrate-helios-lucid.test.ts
+++ tests/integrate-helios-lucid.test.ts
@@ -42,26 +42,19 @@ describe('introduces Helios, a source of a new template', () => {
 	  };
 	}
 
-	const lender = await generateAccount({ lovelace: 75000000000n });
-	const borrower = await generateAccount({ lovelace: 100000000n });
+	const alice = await generateAccount({ lovelace: 75000000000n });
+	const bob = await generateAccount({ lovelace: 100000000n });
 
-	const emulator = new Emulator([lender, borrower]);
+	const emulator = new Emulator([alice , bob]);
 
 	const lucid = await Lucid.new(emulator);
 
 	// emulator state changes, how do I encapsulate this?
-	lucid.selectWalletFromSeed(borrower.seedPhrase);
+	lucid.selectWalletFromSeed(bob.seedPhrase);
 	const zeroState = await lucid.wallet.getUtxos(lucid.wallet.address())
 	// https://github.com/spacebudz/lucid/blob/main/src/examples/matching_numbers.ts
 	
 	const src = await fs.readFile('./src/always-succeeds.hl', 'utf8');
-	// const src = `
-	// spending always_succeeds
-
-	// func main(_, _, _) -> Bool {
-	//     true
-	// }`
-	
 	const program = Program.new(src)
 	const Uplc = program.compile();	
 	const myUplcProgram = JSON.parse(Uplc.serialize());
@@ -69,16 +62,13 @@ describe('introduces Helios, a source of a new template', () => {
 	const matchingNumberScript: SpendingValidator = {
 	  type: "PlutusV2",
 	  // type: myUplcProgram.type,
-	  // script: myUplcProgram.cborHex
 	  script: myUplcProgram.cborHex
 
 	};
 
-
 	const matchingNumberAddress: Address = lucid.utils.validatorToAddress(
 	  matchingNumberScript,
 	);
-
 
 	const Datum = (number: number) => Data.to(BigInt(number));
 	const Redeemer = (number: number) => Data.to(BigInt(number));
@@ -99,20 +89,45 @@ describe('introduces Helios, a source of a new template', () => {
 	  return txHash;
 	}
 
-	await lockUtxo(1,100); 
+	async function redeemUtxo(number: number): Promise<TxHash> {
+		const utxo = (await lucid.utxosAt(matchingNumberAddress)).slice(-1)[0];
+
+		const tx = await lucid
+		.newTx()
+		.collectFrom([utxo], Redeemer(number))
+		.attachSpendingValidator(matchingNumberScript)
+		.complete();
+
+		const signedTx = await tx.sign().complete();
+
+		const txHash = await signedTx.submit();
+
+		return txHash;
+	}
+
+	await lockUtxo(123,100000); 
 	emulator.awaitBlock(4);
 
-	const utxos = await lucid.wallet.getUtxos(
-	  lucid.wallet.address(),
-	);
-	const difference = zeroState[0].assets.lovelace - utxos[0].assets.lovelace;
-	console.log(difference)
-	
-	// if I place this before tx, it shows 100000000n
-	const walletUtxo = await lucid.wallet.getUtxos(matchingNumberAddress)
-	console.log(walletUtxo[0].assets.lovelace);
+	console.log((await lucid.utxosAt(await lucid.wallet.address()))[0]);
+	console.log((await lucid.utxosAt(matchingNumberAddress))[0]);
 
-	return utxos[0].txHash != zeroState[0].txHash
+	await redeemUtxo(1);
+	emulator.awaitBlock(4);
+	// return utxos[0].txHash != zeroState
+	// - [x] test emulator
+	// - [x] test lockUtxo
+	// - [x] test redeemUtxo
+	//
+	// what is my oracle?
+	// console.log((await lucid.utxosAt(alice.address))[0]);
+	// // I am doing this wrong, see #77
+	// console.log((await lucid.utxosAt(matchingNumberAddress))[0]);
+
+	const balance = (await lucid.utxosAt(await lucid.wallet.address()))[0]
+	console.log(zeroState[0].assets.lovelace - balance.assets.lovelace );
+
+	// 99654456n
+	return balance.assets.lovelace == 99654456n
 	} catch (err) {
 	    console.error("something failed:", err);
 	    return false;
@@ -121,16 +136,17 @@ describe('introduces Helios, a source of a new template', () => {
 
 	it('adds logging', async () => {
 
-	const logMsgs = new Set();
-	const logSpy = vi.spyOn(global.console, 'log')
-			 .mockImplementation((msg) => { logMsgs.add(msg); });
+		const logMsgs = new Set();
+		const logSpy = vi.spyOn(global.console, 'log')
+				 .mockImplementation((msg) => { logMsgs.add(msg); });
 
-	let mainStatus = await main();
-	logSpy.mockRestore();
-	if (!mainStatus) {
-	    console.log("Smart Contract Messages: ", logMsgs);
+		let mainStatus = await main();
+		logSpy.mockRestore();
+		if (!mainStatus) {
+		    console.log("Smart Contract Messages: ", logMsgs);
 	}
 	expect(mainStatus).toBe(true);
+	console.log(logMsgs);
 
 	})
 
