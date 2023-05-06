349ecce72ad17f147c46b563f73b270f6b77e057
Alex <alexeusgr@gmail.com>
2023-05-06 11:28:21 +0200

    validator logic

--- helios/loan-escrow.js
+++ helios/loan-escrow.js
@@ -1,5 +1,44 @@
-spending always_succeeds
+spending loan
 
-func main(_, _, _) -> Bool {
-    true
+struct Datum {
+    lender: PubKeyHash
+    borrower: PubKeyHash
+    deadline: Time
+}
+
+enum Redeemer {
+    WithdrawADA
+    WithdrawCol
+}
+
+func main(datum: Datum, redeemer: Redeemer, context: ScriptContext) -> Bool {
+    tx: Tx = context.tx;
+    now: Time = tx.time_range.start;
+
+    redeemer.switch {
+        WithdrawADA => {
+		( // borrower withdraws
+			// Collateral
+			(now < datum.deadline).trace("VS5: ") && 
+
+			// Check that the owner signed the transaction
+			tx.is_signed_by(datum.lender).trace("VS6: "))
+		|| // Lender Cancels
+			( false
+			//
+			)
+        },
+        WithdrawCol => {
+		( // borrower  
+			// Collateral
+			(now < datum.deadline).trace("VS5: ") && 
+
+			// Check that the owner signed the transaction
+			tx.is_signed_by(datum.lender).trace("VS6: "))
+		|| // Lender Cancels
+			( false
+			//
+			)
+        }
+    }
 }
