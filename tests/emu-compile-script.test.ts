db8c36b928de2ad9ff1146420bc68baa893e2d23
Alex <alexeusgr@gmail.com>
2023-05-10 13:41:23 +0200

    indent, remove prints, change l62 to validatorAddress

--- tests/emu-compile-script.test.ts
+++ tests/emu-compile-script.test.ts
@@ -2,6 +2,7 @@ import { describe, expect, it, vi } from 'vitest'
 import { promises as fs } from 'fs';
 import {
   Assets, 
+  Address,
   ConstrData, 
   MintingPolicyHash,
   NetworkEmulator,
@@ -21,65 +22,61 @@ describe('Creates Helios Emulator ... ', () => {
 	const minAda = BigInt(2000000);  // minimum lovelace needed to send an NFT
 	try {
 
-      const network = new NetworkEmulator();
+	const network = new NetworkEmulator();
 
-      const alice = network.createWallet(BigInt(10000000));
+	const alice = network.createWallet(BigInt(10000000));
 
-      network.createUtxo(alice, BigInt(5000000));
+	network.createUtxo(alice, BigInt(5000000));
 
-      const testAsset = new Assets();
-      testAsset.addComponent(
-          MintingPolicyHash.fromHex(
-          '16aa5486dab6527c4697387736ae449411c03dcd20a3950453e6779c'
-          ),
-          Array.from(new TextEncoder().encode('Test Asset Name')),
-          BigInt(1)
-      );
+	const testAsset = new Assets();
+	testAsset.addComponent(
+		MintingPolicyHash.fromHex(
+		'16aa5486dab6527c4697387736ae449411c03dcd20a3950453e6779c'
+		),
+		Array.from(new TextEncoder().encode('Test Asset Name')),
+		BigInt(1)
+	);
 
-      network.createUtxo(alice, BigInt(2000000), testAsset);
+	network.createUtxo(alice, BigInt(2000000), testAsset);
 
-      network.tick(BigInt(10));
+	network.tick(BigInt(10));
 
-      const utxos = await network.getUtxos(alice.address);
+	const utxos = await network.getUtxos(alice.address);
 
-      // Pull in a script, and compile;
-      const script = await fs.readFile('./src/always-succeeds.hl', 'utf8');
-      // https://www.hyperion-bt.org/helios-book/api/reference/program.html?highlight=Program#program
-      const scriptProgram = Program.new(script);
-      const scriptCompiledProgram = scriptProgram.compile(optimize);
-      // https://www.hyperion-bt.org/helios-book/api/reference/uplcprogram.html
-      const scriptValidatorHash = scriptCompiledProgram.validatorHash;
+	// Pull in a script, and compile;
+	const script = await fs.readFile('./src/always-succeeds.hl', 'utf8');
+	// https://www.hyperion-bt.org/helios-book/api/reference/program.html?highlight=Program#program
+	const scriptProgram = Program.new(script);
+	const scriptCompiledProgram = scriptProgram.compile(optimize);
+	// https://www.hyperion-bt.org/helios-book/api/reference/uplcprogram.html
+	const scriptValidatorHash = scriptCompiledProgram.validatorHash;
+	const validatorAddress = Address.fromValidatorHash(scriptValidatorHash);
 
-      const tx = new Tx();
+	const tx = new Tx();
 
-      tx.addInputs(utxos);
+	tx.addInputs(utxos);
 
-      // tx.attachScript(scriptCompiledProgram);
+	// tx.attachScript(scriptCompiledProgram);
 
-      // hm, what happens here?
-      tx.addOutput(new TxOutput(
-	  // https://www.hyperion-bt.org/helios-book/cli/usage.html?highlight=address#calculating-a-script-address
-          alice.address,
-          new Value(minAda)
-        ));
+	tx.addOutput(new TxOutput(
+		validatorAddress,
+		new Value(minAda)
+	));
 
-        // Network Parameters
-      const networkParamsFile = await fs.readFile('./src/preprod.json', 'utf8');
-      const networkParams = new NetworkParams(JSON.parse(networkParamsFile.toString()));
+	// Network Parameters
+	const networkParamsFile = await fs.readFile('./src/preprod.json', 'utf8');
+	const networkParams = new NetworkParams(JSON.parse(networkParamsFile.toString()));
 
-      await tx.finalize(networkParams, alice.address, utxos);
-      const txId = await network.submitTx(tx);
+	await tx.finalize(networkParams, alice.address, utxos);
+	const txId = await network.submitTx(tx);
 
-      // Tick the network on 10 more slots,
-      network.tick(BigInt(10));
+	// Tick the network on 10 more slots,
+	network.tick(BigInt(10));
 
-      const utxosFinal = await network.getUtxos(alice.address);
-      console.log(alice.address);
-      console.log(utxosFinal[0].value.dump());
-      console.log(utxosFinal[1].value.dump());
-      console.log(utxosFinal[2].value.dump());
+	const utxosFinal = await network.getUtxos(validatorAddress);
+	console.log(utxosFinal);
 
-	return utxosFinal[2].value.dump().lovelace == '13754133';
+	return utxosFinal[0].value.dump().lovelace == '2000000'
 	} catch (err) {
 	    console.error("something failed:", err);
 	    return false;
