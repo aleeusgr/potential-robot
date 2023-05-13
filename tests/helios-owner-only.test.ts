5a79321bb0957ee99e6dff63825da5d5da41e836
Alex <alexeusgr@gmail.com>
2023-05-13 08:54:57 +0200

    Address?

--- tests/helios-owner-only.test.ts
+++ tests/helios-owner-only.test.ts
@@ -2,6 +2,7 @@ import { describe, expect, it, vi } from 'vitest'
 import { promises as fs } from 'fs';
 import {
   Assets, 
+  Address,
   ConstrData, 
   MintingPolicyHash,
   NetworkEmulator,
@@ -13,31 +14,38 @@ import {
   Tx, 
 } from "@hyperionbt/helios";
 
-describe('Instantiates the emulator', () => {
+describe('Submits a transaction to a validator address', () => {
 
     const main = async () => {
 
 	let optimize = false;
 	const minAda = BigInt(2000000);  // minimum lovelace needed to send an NFT
 	try {
-	// Create an Instance of NetworkEmulator
-      const network = new NetworkEmulator();
+	const network = new NetworkEmulator();
 
-      // Create a Wallet - we add 10ADA to start
-      const alice = network.createWallet(BigInt(10000000));
+	const alice = network.createWallet(BigInt(10000000));
 
-      // Add an additional lovelace only UTXO
-      network.createUtxo(alice, BigInt(5000000));
+	network.createUtxo(alice, BigInt(5000000));
 
-      // Now lets tick the network on 10 slots,
-      // this will allow the UTxOs to be created from Genisis
-      network.tick(BigInt(10));
+	network.tick(BigInt(10));
 
       // Now we are able to get the UTxOs in Alices wallet
 
+	const script = await fs.readFile('./src/owner-only.hl', 'utf8'); 
+	const program = Program.new(script); 
+	const compiledProgram = program.compile(optimize); 
+
+	const validatorHash = compiledProgram.validatorHash;
+	// https://www.hyperion-bt.org/helios-book/lang/builtins/address.html#address
+	const validatorAddress = Address.fromValidatorHash(validatorHash); 
+	
+
       const utxosFinal = await network.getUtxos(alice.address);
 
-	return utxosFinal[1].value.dump().lovelace == '5000000'
+	// return  compiledProgram.toCbor()
+	// return utxosFinal[1].value.dump().lovelace == '5000000' && validatorHash.hex == '5763a59e90792233f05069b5d4ee16436e7d971ae25f388ec92a5e77'
+	return validatorAddress
+
 	} catch (err) {
 	    console.error("something failed:", err);
 	    return false;
