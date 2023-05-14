0f30441a6fa751cf68c1bd768ee1443ab01419dd
Alex <alexeusgr@gmail.com>
2023-05-14 14:59:27 +0200

    rlf, now I have a tx which sends whole utxo to a validator address. Right?

--- tests/helios-int-emu-scr.test.ts
+++ tests/helios-int-emu-scr.test.ts
@@ -55,17 +55,12 @@ describe('Submits a transaction to a validator address', () => {
 	const txId = await network.submitTx(tx);
 	const utxosFinal = await network.getUtxos(alice.address); // returns a list!!!
 
-	// typeof - object? yea
-	// Why Object.keys are [] ?
-	// https://www.hyperion-bt.org/helios-book/lang/builtins/txid.html?highlight=txid#txid
-	// txId.show() returns False
-	// https://www.hyperion-bt.org/helios-book/api/reference/network.html?highlight=submitTx#submittx
-	// Submits a transaction to the blockchain. Returns the TxId.
-	// https://www.hyperion-bt.org/helios-book/api/reference/txid.html
-	//
-	return txId.hex == 'e91250f6c77da5b50533901a29c75eb9ad6838c7a2044435675966c67cbddc93'
-	// return tx.dump().body.outputs[0].value.lovelace == '9836215'
-	// return utxosFinal[1].value.dump().lovelace == '5000000' && validatorAddress.toBech32() == 'addr_test1wq8jn3u0ts654lp6ltvyju7nflcm5qegukqukuhc4jdxhag7ku5n4'
+	// ok, this is why I need BeforeAll, BeforeEach, and it? 
+	// Anyway, let me think about redundancy here;
+	// But it starts to get long. 
+	// these values will change if I change L36
+	// Ok, commit these comments before going on
+	return utxosFinal[1].value.dump().lovelace == '5000000' && validatorAddress.toBech32() == 'addr_test1wq8jn3u0ts654lp6ltvyju7nflcm5qegukqukuhc4jdxhag7ku5n4' && txId.hex == 'e91250f6c77da5b50533901a29c75eb9ad6838c7a2044435675966c67cbddc93' && tx.dump().body.outputs[0].value.lovelace == '9836215'
 
 	} catch (err) {
 	    console.error("something failed:", err);
