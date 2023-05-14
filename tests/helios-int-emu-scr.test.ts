5acddacd57967332059ff1262783eba83181a609
Alex <alexeusgr@gmail.com>
2023-05-14 14:46:38 +0200

    doc

--- tests/helios-int-emu-scr.test.ts
+++ tests/helios-int-emu-scr.test.ts
@@ -55,6 +55,9 @@ describe('Submits a transaction to a validator address', () => {
 	const txId = await network.submitTx(tx);
 	const utxosFinal = await network.getUtxos(alice.address); // returns a list!!!
 
+	// typeof - object? yea
+	// Why Object.keys are [] ?
+	// https://www.hyperion-bt.org/helios-book/lang/builtins/txid.html?highlight=txid#txid
 	return txId
 	// return tx.dump().body.outputs[0].value.lovelace == '9836215'
 	// return utxosFinal[1].value.dump().lovelace == '5000000' && validatorAddress.toBech32() == 'addr_test1wq8jn3u0ts654lp6ltvyju7nflcm5qegukqukuhc4jdxhag7ku5n4'
