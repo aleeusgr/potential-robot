66fcfd73056618f55bbfa5399b1a09b8de87e89e
Alex <alexeusgr@gmail.com>
2023-05-17 11:07:41 +0200

    style(tests/helios-program): describe it
    what does a test do?

--- tests/helios-program.test.ts
+++ tests/helios-program.test.ts
@@ -16,17 +16,17 @@ describe("provide a uplc, an instance of a plutus script",async () => {
 	const validatorHash = compiledProgram.validatorHash;
 	// https://www.hyperion-bt.org/helios-book/lang/builtins/address.html#address
 	const validatorAddress = Address.fromValidatorHash(validatorHash); 
-	console.log(validatorHash.hex.length)
 
+	it ("checks methods", async () => {
+	// https://www.hyperion-bt.org/helios-book/api/reference/program.html	
+	  expect(program.name).toBe('owner_only')
+	})
+	it ("checks Properties", async () => {
+	//https://www.hyperion-bt.org/helios-book/api/reference/validatorhash.html
+	  expect(validatorHash.hex.length).toBe(56);
+	})
 	it ("documents validatorAddress", async () => {
 // https://github.com/lley154/helios-examples/blob/704cf0a92cfe252b63ffb9fd36c92ffafc1d91f6/vesting/pages/index.tsx#LL370C43-L370C43
 	  expectTypeOf(validatorAddress.toBech32).toMatchTypeOf("string")
 	})
-	it ("checks Program", async () => {
-	  expect(false).toBeTruthy()
-	})
-	it ("checks validatorHash ", async () => {
-	//https://www.hyperion-bt.org/helios-book/api/reference/validatorhash.html
-	  expect(validatorHash.hex.length).toBe(56);
-	})
-	})
+})
