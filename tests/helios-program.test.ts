62722177397263abd3d67e8e0a9fab3f2ba3f7f2
Alex <alexeusgr@gmail.com>
2023-05-17 10:00:35 +0200

    feat(helios-program.test): copy suggested template and make test pass
    I obtain building blocks, how granular they should be?

--- tests/helios-program.test.ts
+++ tests/helios-program.test.ts
@@ -1,1 +1,17 @@
 // first I need to choose a template
+// function to provide a uplc, an instance of a plutus script;
+import { describe, expect, it, beforeEach, vi } from 'vitest'
+
+describe("vesting contract", () => {
+
+	let vestingProgram, initiatorWallet, recipientWallet;
+	// I should add a test that shows how this works;
+	beforeEach(() => { 
+	// vestingProgram = new helios.Program( ... ); 
+	// ...
+	console.log();
+	})
+	it ("tests things while reusing provided `vestingProgram`", async () => {
+	  return true
+	})
+})
