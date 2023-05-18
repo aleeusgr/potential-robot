803e0a36b379824e227d1cc5d581bb56d7c3b148
Alex <alexeusgr@gmail.com>
2023-05-18 08:25:50 +0200

    feat(helios-program-doc.test.ts): added beforeEach to the test

--- tests/helios-program-doc.test.ts
+++ tests/helios-program-doc.test.ts
@@ -1,30 +1,38 @@
-import { describe, expect, it, expectTypeOf, vi } from 'vitest'
+import { describe, expect, it, expectTypeOf, beforeEach, vi } from 'vitest'
 import { promises as fs } from 'fs';
 import {
   Address,
   Program, 
 } from "@hyperionbt/helios";
 
-describe("provide a uplc, an instance of a plutus script",async () => {
+describe("a uplc instance is created from source code", async () => {
 
-	let optimize = false;
+	// https://vitest.dev/guide/test-context.html
+	beforeEach(async (context) => { 
+		let optimize = false;
 
-	const script = await fs.readFile('./src/owner-only.hl', 'utf8'); 
-	const program = Program.new(script); 
-	const compiledProgram = program.compile(optimize); 
-	const validatorHash = compiledProgram.validatorHash;
-	// https://www.hyperion-bt.org/helios-book/lang/builtins/address.html#address
-	const validatorAddress = Address.fromValidatorHash(validatorHash); 
+		const script = await fs.readFile('./src/matching-keyhash.js', 'utf8'); 
+		const program = Program.new(script); 
+		const compiledProgram = program.compile(optimize); 
+		const validatorHash = compiledProgram.validatorHash;
+		// https://www.hyperion-bt.org/helios-book/lang/builtins/address.html#address
+		const validatorAddress = Address.fromValidatorHash(validatorHash); 
+	 
+		context.validatorHash = validatorHash;
+		context.validatorAddress = Address.fromValidatorHash(validatorHash); 
+		context.programName = program.name;
 
-	it ("checks methods", async () => {
-	// https://www.hyperion-bt.org/helios-book/api/reference/program.html	
-	  expect(program.name).toBe('owner_only')
 	})
-	it ("checks Properties", async () => {
+
+	it ("checks methods", async ({programName}) => {
+	// https://www.hyperion-bt.org/helios-book/api/reference/program.html	
+	  expect(programName).toBe('matching_pubKeyHash')
+	})
+	it ("checks Properties", async ({validatorHash}) => {
 	//https://www.hyperion-bt.org/helios-book/api/reference/validatorhash.html
 	  expect(validatorHash.hex.length).toBe(56);
 	})
-	it ("documents validatorAddress", async () => {
+	it ("documents API usage", async ({validatorAddress}) => {
 // https://github.com/lley154/helios-examples/blob/704cf0a92cfe252b63ffb9fd36c92ffafc1d91f6/vesting/pages/index.tsx#LL370C43-L370C43
 	  expectTypeOf(validatorAddress.toBech32).toMatchTypeOf("string")
 	})
