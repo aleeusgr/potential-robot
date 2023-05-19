1c6dcc817f7d4cea58c48a64b61f44ccf7223f2d
Alex <alexeusgr@gmail.com>
2023-05-19 08:08:10 +0200

    feat(helios-vesting.test): building the transaction
    now need to add validatorAddress to the scope

--- tests/helios-vesting.test.ts
+++ tests/helios-vesting.test.ts
@@ -2,13 +2,18 @@ import { describe, expect, it, expectTypeOf, beforeEach, vi } from 'vitest'
 import { promises as fs } from 'fs';
 import {
   Address,
+  Assets,
   ByteArrayData,
+  ConstrData,
   Datum,
+  hexToBytes,
   IntData,
   ListData,
   NetworkEmulator,
   NetworkParams,
   Program, 
+  Tx,
+  TxOutput,
   Value,
 } from "@hyperionbt/helios";
 
@@ -73,8 +78,68 @@ describe("a vesting contract", async () => {
 		const datum = new ListData([new ByteArrayData(ownerPkh.bytes),
 					    new ByteArrayData(benPkh.bytes),
 					    new IntData(BigInt(deadline.getTime()))]);
-
 		const inlineDatum = Datum.inline(datum);
+
+		const inputUtxos = await alice.utxos;
+
+		const tx = new Tx();
+
+		tx.addInputs(inputUtxos);
+
+		const mintScript =`minting nft
+
+		const TX_ID: ByteArray = #` + inputUtxos[0].txId.hex + `
+		const txId: TxId = TxId::new(TX_ID)
+		const outputId: TxOutputId = TxOutputId::new(txId, ` + inputUtxos[0].utxoIdx + `)
+
+		func main(ctx: ScriptContext) -> Bool {
+			tx: Tx = ctx.tx;
+			mph: MintingPolicyHash = ctx.get_current_minting_policy_hash();
+
+			assetclass: AssetClass = AssetClass::new(
+			mph,
+			"Vesting Key".encode_utf8()
+			);
+			value_minted: Value = tx.minted;
+
+			// Validator logic starts
+			(value_minted == Value::new(assetclass, 1)).trace("NFT1: ") &&
+			tx.inputs.any((input: TxInput) -> Bool {
+						(input.output_id == outputId).trace("NFT2: ")
+						}
+			)
+		}`
+
+		const optimize = false;
+		const mintProgram = Program.new(mintScript).compile(optimize);
+
+		tx.attachScript(mintProgram);
+
+		// Construct the NFT that we will want to send as an output
+		const nftTokenName = ByteArrayData.fromString("Vesting Key").toHex();
+		const tokens: [number[], bigint][] = [[hexToBytes(nftTokenName), BigInt(1)]];
+
+		// Create an empty Redeemer because we must always send a Redeemer with
+		// a plutus script transaction even if we don't actually use it.
+		const mintRedeemer = new ConstrData(0, []);
+
+		// Indicate the minting we want to include as part of this transaction
+		tx.mintTokens(
+		mintProgram.mintingPolicyHash,
+		tokens,
+		mintRedeemer
+		)
+
+		const lockedVal = new Value(adaAmountVal.lovelace, new Assets([[mintProgram.mintingPolicyHash, tokens]]));
+
+
+		// Add the destination address and the amount of Ada to lock including a datum
+		// tx.addOutput(new TxOutput(valAddr, lockedVal, inlineDatum));
+
+		// Add the collateral
+		// tx.addCollateral(colatUtxo);
+
+		expect().toBe();
 	
 
 	})
