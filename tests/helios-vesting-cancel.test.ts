d447bfcb12bdf236d4bc0ca80f903c1aef28cb3e
Alex <alexeusgr@gmail.com>
2023-05-21 09:37:50 +0200

    feat(helios-vesting-cancel.test): added boilerplate

---
+++ tests/helios-vesting-cancel.test.ts
@@ -0,0 +1,65 @@
+import { describe, expect, it, expectTypeOf, beforeEach, vi } from 'vitest'
+import { promises as fs } from 'fs';
+import {
+  Address,
+  Assets,
+  ByteArrayData,
+  ConstrData,
+  Datum,
+  hexToBytes,
+  IntData,
+  ListData,
+  NetworkEmulator,
+  NetworkParams,
+  Program, 
+  Tx,
+  TxOutput,
+  Value,
+} from "@hyperionbt/helios";
+import {lockAda} from './src/lockAda.ts';
+
+describe("a vesting contract: Cancel transaction", async () => {
+
+	// https://vitest.dev/guide/test-context.html
+	beforeEach(async (context) => { 
+		let optimize = false;
+
+		// compile script
+		const script = await fs.readFile('./src/vesting.js', 'utf8'); 
+		const compiledProgram = Program.new(script).compile(optimize); 
+		const validatorHash = compiledProgram.validatorHash;
+		const validatorAddress = Address.fromValidatorHash(validatorHash); 
+	 
+		context.validatorHash = validatorHash;
+		context.validatorAddress = Address.fromValidatorHash(validatorHash); 
+
+		// instantiate the Emulator
+		const minAda = BigInt(2000000);  // minimum lovelace needed to send an NFT
+		const network = new NetworkEmulator();
+
+		const alice = network.createWallet(BigInt(20000000));
+		network.createUtxo(alice, BigInt(5000000));
+		const bob = network.createWallet(BigInt(10000000));
+		network.tick(BigInt(10));
+
+		context.alice = alice;
+		context.bob = bob;
+		context.network = network;
+
+	})
+
+	it ("docs the tx ingridients", async ({network, alice, validatorHash}) => {
+		// https://www.hyperion-bt.org/helios-book/api/reference/address.html?highlight=Address#address
+		const aliceUtxos = await network.getUtxos(alice.address);
+		// todo
+		expect(alice.address.toHex().length).toBe(58)
+		// todo
+		expect(aliceUtxos[1].value.dump().lovelace).toBe('5000000')
+		// todo
+		expect(validatorHash.hex).toBe('0502e977b1b2d1be41edabd19401d65d43f1d936f82297b72c71663c')
+	})
+
+	it ("adds new code", async ({network, alice, validatorHash}) => {
+		expect().toBe();
+	})
+})
