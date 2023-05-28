55cd811cab27a338c77f3a0b7728611df2190845
Alex <alexeusgr@gmail.com>
2023-05-28 09:27:38 +0200

    feat(network-params.test): init emu

--- tests/helios-networkParams-doc.test.ts
+++ tests/helios-networkParams-doc.test.ts
@@ -2,37 +2,38 @@ import { describe, expect, it, expectTypeOf, beforeEach, vi } from 'vitest'
 import { promises as fs } from 'fs';
 import {
   Address,
+  NetworkEmulator,
   Program, 
 } from "@hyperionbt/helios";
 
-describe("a uplc instance is created from source code", async () => {
+describe("run a transaction on newly initialized params", async () => {
 
 	// https://vitest.dev/guide/test-context.html
 	beforeEach(async (context) => { 
-		let optimize = false;
+		// instantiate the Emulator
+		// const minAda = BigInt(2000000);  // minimum lovelace needed to send an NFT
+		const network = new NetworkEmulator();
 
-		const script = await fs.readFile('./src/vesting.js', 'utf8'); 
-		const program = Program.new(script); 
-		const compiledProgram = program.compile(optimize); 
-		const validatorHash = compiledProgram.validatorHash;
-		// https://www.hyperion-bt.org/helios-book/lang/builtins/address.html#address
-		const validatorAddress = Address.fromValidatorHash(validatorHash); 
-	 
-		context.validatorHash = validatorHash;
-		context.validatorAddress = Address.fromValidatorHash(validatorHash); 
-		context.programName = program.name;
+		const alice = network.createWallet(BigInt(20000000));
+		network.createUtxo(alice, BigInt(5000000));
+		const bob = network.createWallet(BigInt(10000000));
+		network.tick(BigInt(10));
+
+		context.alice = alice;
+		context.bob = bob;
+		context.network = network;
 
 	})
 
-	it ("checks methods", async ({programName}) => {
-	// https://www.hyperion-bt.org/helios-book/api/reference/program.html	
-	  expect(programName).toBe('vesting')
+	it ("checks methods", async ({alice}) => {
+	// https://www.hyperion-bt.org/helios-book/api/reference/address.html
+	  expect(alice.address.toBech32()).toBe('addr_test1vz2a83z0d5gceyghfrjqpaqu2f98evk8q6swje2c5ddqmaca2vhfl')
 	})
-	it ("checks Properties", async ({validatorHash}) => {
+	it.skip("checks Properties", async ({validatorHash}) => {
 	//https://www.hyperion-bt.org/helios-book/api/reference/validatorhash.html
 	  expect(validatorHash.hex.length).toBe(56);
 	})
-	it ("documents API usage", async ({validatorAddress}) => {
+	it.skip("documents API usage", async ({validatorAddress}) => {
 // https://github.com/lley154/helios-examples/blob/704cf0a92cfe252b63ffb9fd36c92ffafc1d91f6/vesting/pages/index.tsx#LL370C43-L370C43
 	  expectTypeOf(validatorAddress.toBech32).toMatchTypeOf("string")
 	})
