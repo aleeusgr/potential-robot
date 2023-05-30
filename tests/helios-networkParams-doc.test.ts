1abc64d8fd047088cf5e39da67f128ed47078c03
Alex <alexeusgr@gmail.com>
2023-05-30 07:36:45 +0200

    test(network-Params-doc.test): added test for time and slot values parsed from json

--- tests/helios-networkParams-doc.test.ts
+++ tests/helios-networkParams-doc.test.ts
@@ -24,20 +24,22 @@ describe("run a transaction on newly initialized params", async () => {
 
 	})
 
-	it ("checks methods", async ({alice, network}) => {
+	it ("checks networkParams parsing", async ({network}) => {
 	// https://www.hyperion-bt.org/helios-book/api/reference/address.html
-		expect(alice.address.toBech32()).toBe('addr_test1vz2a83z0d5gceyghfrjqpaqu2f98evk8q6swje2c5ddqmaca2vhfl')
 		const networkParamsFile = await fs.readFile('./src/preprod.json', 'utf8');
-		const networkParams = new NetworkParams(JSON.parse(networkParamsFile.toString()));
+		const networkParamsJSON = JSON.parse(networkParamsFile.toString());
+		const networkParams = new NetworkParams(networkParamsJSON);
+		expect(networkParamsJSON.latestTip.slot).toBe(21425784);
+		expect(networkParamsJSON.latestTip.time).toBe(1677108984000);
+		expect(networkParams.slotToTime(BigInt(networkParamsJSON.latestTip.slot))).toBe(BigInt(networkParamsJSON.latestTip.time));
+	})
+
+	it ("checks network.initNetworkParams", async ({network}) => {
+	// https://www.hyperion-bt.org/helios-book/api/reference/address.html
+		const networkParamsFile = await fs.readFile('./src/preprod.json', 'utf8');
+		const networkParamsJSON = JSON.parse(networkParamsFile.toString());
+		const networkParams = new NetworkParams(networkParamsJSON);
 		const newNP = network.initNetworkParams(networkParams)
 		expect(newNP.slotToTime(BigInt(0))).toBe(1655683200000n);
-	})
-	it.skip("checks Properties", async ({validatorHash}) => {
-	//https://www.hyperion-bt.org/helios-book/api/reference/validatorhash.html
-	  expect(validatorHash.hex.length).toBe(56);
-	})
-	it.skip("documents API usage", async ({validatorAddress}) => {
-// https://github.com/lley154/helios-examples/blob/704cf0a92cfe252b63ffb9fd36c92ffafc1d91f6/vesting/pages/index.tsx#LL370C43-L370C43
-	  expectTypeOf(validatorAddress.toBech32).toMatchTypeOf("string")
 	})
 })
