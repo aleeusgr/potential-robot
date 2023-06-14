ac4050a12cf8a82586012d165f13b6a9f994f2d2
Alex <alexeusgr@gmail.com>
2023-06-14 18:33:18 +0200

    feat(tests/fuzzy): document Helios method
    BREAKING CHANGE: #123

---
+++ tests/unit/fuzzy.test.ts
@@ -0,0 +1,29 @@
+import { describe, expect, it, expectTypeOf, beforeEach, vi } from 'vitest'
+import { promises as fs } from 'fs';
+import {
+	Assets,
+	FuzzyTest,
+	MintingPolicyHash,
+	NetworkEmulator,
+	Program
+} from "@hyperionbt/helios";
+
+
+import {lockAda} from './src/lockAda.ts';
+
+describe("create a network with two wallets of which one has an nft", async () => {
+
+	// https://vitest.dev/guide/test-context.html
+	beforeEach(async (context) => {
+
+	})
+
+	it ("checks for Fuzzy", async ({network, alice, bob, mph}) => {
+		// https://www.hyperion-bt.org/helios-book/api/reference/fuzzytest.html?highlight=fuzzy#fuzzytest
+		const fuzzy = new FuzzyTest(0,100,false)
+		expect(await fuzzy.int(0)).toBe();
+
+
+	})
+
+})
