194c04d47a71aefaafbfc4ef9b0f6b6053366aa8
Alex <alexeusgr@gmail.com>
2023-06-15 07:16:48 +0200

    fix: disable fuzzy unit test and fix contract name in time-props

--- tests/unit/fuzzy.test.ts
+++ tests/unit/fuzzy.test.ts
@@ -18,7 +18,7 @@ describe("create a network with two wallets of which one has an nft", async () =
 
 	})
 
-	it ("checks for Fuzzy", async ({network, alice, bob, mph}) => {
+	it.fails ("checks for Fuzzy", async ({network, alice, bob, mph}) => {
 		// https://www.hyperion-bt.org/helios-book/api/reference/fuzzytest.html?highlight=fuzzy#fuzzytest
 		const fuzzy = new FuzzyTest(0,100,false)
 		expect(await fuzzy.int(0)).toBe();
